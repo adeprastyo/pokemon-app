1e535f51cc9ede4f6ade5ed40c2a63898aed14e9
adeprastyo <adeprastyo101@gmail.com>
2024-03-08 10:39:14 +0700

    feat: init repo pokemon app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center text-white">
+      <div className="w-1/3 bg-slate-700 h-dvh flex justify-center items-center overflow-auto">
+        <p className="text-2xl">Pokemon App</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
